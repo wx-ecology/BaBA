a9c7349b90bf5c7fc3cd640406f5840cb767c000
wx-ecology <wenjing.xuuu@gmail.com>
2020-07-06 18:15:49 -0700

    BaRanking - if not all behaviors are present

--- R/BaRanking.R
+++ R/BaRanking.R
@@ -19,7 +19,18 @@ BaRanking <- function(classification, barrier, d, min_total_enc = 0,
   by_type <- as.data.frame(barrier_sf_joined)[!is.na(barrier_sf_joined$AnimalID),]  %>% group_by(FID_Fence0, eventTYPE, .drop = F) %>% 
     summarise(count = n())
   
-  by_type_df <- as.data.frame(by_type) %>% select(FID_Fence0, eventTYPE, count) %>% pivot_wider(names_from = eventTYPE, values_from = count)
+  by_type_df <- as.data.frame(by_type) %>% select(FID_Fence0, eventTYPE, count) %>% pivot_wider(names_from = eventTYPE, values_from = count) %>% replace(is.na(.), 0)
+  
+  # find out whether all behavioral types are listed and added the column into the dataframe and fill with 0
+  if (length(names(by_type_df)) < 8) {
+    ba.names <- names(by_type_df)[2:length(names(by_type_df))]
+    ba.all <- c("Quick_Cross", "Bounce", "Average_Movement", "Back_n_forth", "Trace", "Trapped", "unknown") # a full list of names
+    ba.miss <- ba.all[!(ba.all %in% ba.names)]
+    n.miss <- length(ba.miss)
+    add.df <- as.data.frame(matrix(0, nrow = nrow(by_type_df), ncol = n.miss))
+    colnames(add.df) <- ba.miss
+    by_type_df <- cbind(by_type_df, add.df)
+  }
   
   # calculate # of unique invidiuals interacted with each fence
   by_ID <- as.data.frame(barrier_sf_joined)[!is.na(barrier_sf_joined$AnimalID),]  %>% 
@@ -27,8 +38,8 @@ BaRanking <- function(classification, barrier, d, min_total_enc = 0,
     summarise(unique_ind = length(unique(AnimalID))) 
   
   # combine the two dataframes and calculate total encounters and total bad encounters
-  barrier_sf_joined <- by_ID %>% 
-    left_join(by_type_df, by = "FID_Fence0") %>% 
+  barrier_sf_joined <- by_type_df %>% 
+    left_join(by_ID, by = "FID_Fence0") %>% 
     replace(is.na(.), 0) %>%
     mutate(total_enc = Bounce + Quick_Cross + Average_Movement + Back_n_forth + Trace + unknown + Trapped) 
   
@@ -37,8 +48,6 @@ BaRanking <- function(classification, barrier, d, min_total_enc = 0,
   
   # put back into spatial
   barrier_sf_joined <- merge(barrier_sf, barrier_sf_joined, by = "FID_Fence0", all.x = T)
-  barrier_sf_joined$index[is.na(barrier_sf_joined$index)] <- 0
-  
   
   if(show_plot) {
     plot(st_geometry(barrier_sf), col = "grey")
